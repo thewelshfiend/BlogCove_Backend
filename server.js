eb74957db6db86005dd9147056dddefb6b69f96e
Kiran TJ <kiran.thomas.jacob@gmail.com>
2025-01-14 17:40:32 +0530

    commit 'secureFalse httpTrue'

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         maxAge: 60 * 60 * 1000,  // Session will expire after 1 hour from login if not re-sent
-        secure: true,
+        secure: false,
         httpOnly: true,
         sameSite: "none"
     }
