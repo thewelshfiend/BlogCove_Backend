78582790f924f30f4268317ea99f171339f4d31b
Kiran TJ <kiran.thomas.jacob@gmail.com>
2025-01-14 17:19:17 +0530

    commit 'true'

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(session({
     cookie: {
         maxAge: 60 * 60 * 1000,  // Session will expire after 1 hour from login if not re-sent
         secure: false,
-        sameSite: "none"
+        sameSite: "true"
     }
 }));
 
