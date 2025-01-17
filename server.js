33b3c020d594745d93b48e20d5f83fc9d6e1afa8
Kiran TJ <kiran.thomas.jacob@gmail.com>
2025-01-17 19:19:25 +0530

    Combo fixed

--- server.js
+++ server.js
@@ -35,10 +35,8 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         maxAge: 60 * 60 * 1000,  // Session will expire after 1 hour from login if not re-sent
-        secure: true,
+        secure: false,
         sameSite: "lax"
-        // secure: false,
-        // sameSite: "lax"
     }
 }));
 // APIs
